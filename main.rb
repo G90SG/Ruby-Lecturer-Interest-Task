73287b0a0fe49388de800b006a0e28de65d020a7
G90SG <71718345+G90SG@users.noreply.github.com>
2021-02-22 17:24:56 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -8,19 +8,21 @@
 # Imports 'csv' library
   require 'csv'
   table = []
+# Using 'CSV.foreach' reads the file row by row using '.parse' would have returned an array of arrays
 CSV.foreach("RubyLecturers.csv") do |row|
   table << row
 end
 
-# 1 display the 2nd and 4th area of interest for each lecturer
+# Display the 2nd and 4th area of interest for each lecturer by using 'table[][]' function and entering the index number of the row in the first box (starting at 0,1,2,3 etc) then the column (again starting at 0,1,2,3 etc ). This is called using interpolation within the string
+puts "\n#{table[0][0]}'s second area of interest is #{table[0][2]} and fourth area of interest is #{table[0][4]}. "
 puts "\n#{table[1][0]}'s second area of interest is #{table[1][2]} and fourth area of interest is #{table[1][4]}. "
 puts "\n#{table[2][0]}'s second area of interest is #{table[2][2]} and fourth area of interest is #{table[2][4]}. "
 puts "\n#{table[3][0]}'s second area of interest is #{table[3][2]} and fourth area of interest is #{table[3][4]}. "
 puts "\n#{table[4][0]}'s second area of interest is #{table[4][2]} and fourth area of interest is #{table[4][4]}. "
-puts "\n#{table[5][0]}'s second area of interest is #{table[5][2]} and fourth area of interest is #{table[5][4]}. "
-# 2 show each lecturers final area of interest 
-puts "\n #{table[1][0]}'s final are of interest is #{table[1][-1]}"
+puts "\n"
+# Using 'table[][]' as above, find out each lecturers ,final area of interest, note that on those lines which have fewer entries in the CSV have -2 rather than -1, to skip over the last entry which on those lines would be blank
+puts "\n #{table[0][0]}'s final are of interest is #{table[0][-1]}"
+puts "\n #{table[1][0]}'s final are of interest is #{table[1][-2]}"
 puts "\n #{table[2][0]}'s final are of interest is #{table[2][-1]}"
 puts "\n #{table[3][0]}'s final are of interest is #{table[3][-1]}"
-puts "\n #{table[4][0]}'s final are of interest is #{table[4][-1]}"
-#puts "\n #{table[5][0]}'s final are of interest is #{table[5][-1]}"
+puts "\n #{table[4][0]}'s final are of interest is #{table[4][-2]}"
